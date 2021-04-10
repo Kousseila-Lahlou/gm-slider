40999b1c042dc7656fd589e9389e8f253fde56ed
Kousseila Lahlou <koussie95@gmail.com>
2021-04-10 11:34:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var images = ['im01.jpg','im02.jpg','im03.jpg','im04.jpg','im05.jpg',]
+
+var cpt = 0
+
+function change () {
+    var img = document.getElementById('img-slider')
+    img.setAttribute('src','images/' + images[cpt])
+}
+
+setInterval(() => {
+    change();
+    cpt++;
+    if (cpt>4) cpt=0;
+}, 4000)
+
+function next() {
+    cpt++;
+    if (cpt > 4) cpt = 0;
+    change()
+    
+}
+
+function previous() {
+    cpt--;
+    if (cpt < 0) cpt = 4;
+    change ()
+}
